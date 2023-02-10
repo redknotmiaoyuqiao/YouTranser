349f110884a34bff1658aea598bca8055c1c57b8
redknot <redknot@126.com>
2023-02-10 22:56:32 +0800

    rename project

--- tools/package_unix_mac.sh
+++ tools/package_unix_mac.sh
@@ -2,3 +2,19 @@ cd ../
 
 basepath=$(cd `dirname $0`; pwd)
 echo ${basepath}
+
+cd ${basepath}/YouPicker_QT/
+
+if [ -d ${basepath}/YouPicker_QT/Release_build ];then
+    rm -rf Release_build/
+fi
+
+mkdir Release_build/
+
+cmake ../
+
+make
+
+cd YouTranscoder/
+
+macdeployqt YouTranser.app -dmg
